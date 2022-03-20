e4770d5c874624bbc72ea9877299fcac0b1895cf
EfrenOR <efren.or@outlook.com>
2022-03-19 20:20:22 -0600

    Inicio y primer form terminado en C#

---
+++ Metodos Numericos/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodos_Numericos
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public int xClick = 0, yClick = 0;
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        
+
+        private void panel4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                xClick = e.X; yClick = e.Y;
+            }
+            else
+            {
+                this.Left = this.Left + (e.X - xClick); this.Top = this.Top + (e.Y - yClick);
+            }
+        }
+    }
+}
